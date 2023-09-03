8de5634e39bd21f9ad2d9dbfb6cb97f467f81254
Minindu20 <mininduh.20@cse.mrt.ac.lk>
2023-09-03 21:09:55 +0530

    Login completed

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -8,30 +8,66 @@ import { useNavigate } from "react-router-dom";
 const Login = () => {
   const navigate =useNavigate();
   const [action, setAction] = useState("Sign Up");
+  const [userNameExistMessage, setUserNameExistMessage] = useState("");
+  const [emailExistMessage, setEmailExistMessage] = useState("");
+  const [emailPasswordIncorrectMeasseage, setemailPasswordIncorrectMeasseage] = useState("");
   const [values, setValues] = useState({
     name: "",
     role:"reader",
     email: "",
     password: "",
   });
+  const formData ={...values,mode:action}
   const handleInput = (event) => {
     setValues((prev) => ({
       ...prev,
       [event.target.name]: event.target.value,
     }));
   };
-  const [errorMsg,setErrorMsg]=useState("");
+
   const handleSubmit = async (event) => {
-       event.preventDefault();
-       axios.post("http://localhost:4000/login",values)
-       .then(response =>{
-         console.log(response);
-         navigate('/user')
-       })
-       .catch((error) =>{
-        console.log(error)
-       } );
-  }
+    setUserNameExistMessage("");
+    setEmailExistMessage("");
+    setemailPasswordIncorrectMeasseage("");
+        console.log("forum submitted");
+       if(action ==="Sign Up"){
+        event.preventDefault();
+        axios.post("http://localhost:4000/login",formData)
+        .then(response =>{
+          console.log(response);
+          // if(response.data.nameExist){
+          //   alert("UserName exist")
+          // }else if(response.data.emailExist){
+          //   alert("email exist")
+          if (response.data.nameExist) {
+            setUserNameExistMessage("Username already exists");
+          } else if (response.data.emailExist) {
+            setEmailExistMessage("Email already exists");
+          }else{
+          navigate('/user')
+          }
+        })
+        .catch((error) =>{
+         console.log(error)
+        } );
+       }
+       else{
+        event.preventDefault();
+        axios.post("http://localhost:4000/login",formData)
+        .then(response =>{
+         if(response.data.Login){
+          const route=response.data.route;
+          navigate(route)
+         }else{
+          //alert("No record")
+          setemailPasswordIncorrectMeasseage("Incorrect Username or Password");
+         }
+         console.log(response)
+        })
+        .catch((error) =>{
+          console.log(error)
+         } );
+  }}
   return (
     <>
       <Navbar />
@@ -82,6 +118,9 @@ const Login = () => {
                   />
                 </div>
               </div>
+              {userNameExistMessage && <div className="errorMsg">{userNameExistMessage}</div>}
+              {emailExistMessage && <div className="errorMsg">{emailExistMessage}</div>}
+              {emailPasswordIncorrectMeasseage && <div className="errorMsg">{emailPasswordIncorrectMeasseage}</div>}
               <div className="submit-container">
                 <button
                   className={action === "Login" ? "submit gray" : "submit"}
@@ -106,7 +145,6 @@ const Login = () => {
           </div>
         </div>
       </form>
-
       <Footer />
     </>
   );
