cf16c3f219d468988a857a469b192c4c98a4c7e2
Minindu20 <mininduh.20@cse.mrt.ac.lk>
2023-09-08 19:05:56 +0530

    phase-1

---
+++ src/components/Header/SearchForm.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {FaSearch} from "react-icons/fa";
+import "./SearchForm.css";
+const SearchForm = () => {
+  return (
+    <div className="search-form1">
+      <div className="container">
+        <div className="search-form-content">
+          <form className="search-form">
+            <div className="search-form-element">
+              <input type="text" className="form-control" placeholder="The Lost World..."/>
+              <button type="submit" className="btnClass">
+              <FaSearch className='text-purple' size = {32} />
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchForm;
