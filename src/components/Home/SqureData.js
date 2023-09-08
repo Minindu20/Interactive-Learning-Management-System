fac7b2cc7bff97a3015990b65408cab397f4a330
Minindu20 <mininduh.20@cse.mrt.ac.lk>
2023-09-08 19:48:01 +0530

    sections

--- src/components/Home/SqureData.js
+++ src/components/Home/SqureData.js
@@ -1,13 +1,16 @@
 import "./SqureStyle.css";
 function SqureData(props){
     return(
-        <div className="t-card">
+        <div className="sqr">
+              <div className="t-card">
            <div className="t-image">
             <img src={props.image} alt="image"/>
            </div>
            <h4>{props.heading}</h4>
            <p>{props.text}</p>
         </div>
+        </div>
+      
     )
 }
 export default SqureData;
--- src/routes/User.jsx
+++ src/routes/User.jsx
@@ -1,21 +1,48 @@
-import React from 'react'
-import "./Css/User.css"
-import Sidebar from '../components/Dashboard/Sidebar';
-import Hero from "../components/Home/Hero"
-import Nav from '../components/Dashboard/Nav';
-import search from "../assests/search.jpg"
+import React from "react";
+import "./Css/User.css";
+import Sidebar from "../components/Dashboard/Sidebar";
+import Hero from "../components/Home/Hero";
+import Nav from "../components/Dashboard/Nav";
+import Header from "../components/Header/Header";
+import search from "../assests/search.jpg";
+import Squre from "../components/Home/Squre";
+import BookCard from "../components/Header/BookCard";
+import bk1 from '../assests/books/bk3.jpg'
+import book1 from '../assests/books/bk1.jpg'
+import {Books} from './BookData'
 const User = () => {
   return (
     <div className="user">
-        <Sidebar
-        role="user"/>
-       <div className='userContainer'>
-          <Nav
-          role="user"/>
-         
-       </div>
+      <Sidebar role="user" />
+      <div className="userContainer">
+        <Nav role="user" />
+        <Header />
+        <div className="booklist">
+          <div className="content-title">
+            <h1>Popular</h1>
+            <p>Find our popular books</p>
+            <div className="grid-container">
+              {Books.map((item,index)=>{
+               return(
+                <div className ="grid-item" key={index}>
+                   <BookCard
+                    image={item.image}
+                    heading={item.title}
+                    author={item.author}
+                    />
+                </div>
+               )
+              })}
+            </div>
+          </div>
+          <div className="content-title">
+            <h1>Top pics</h1>
+            <p>Find our Top Pics</p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default User
+export default User;
