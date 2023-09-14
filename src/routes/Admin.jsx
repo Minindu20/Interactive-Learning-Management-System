3ae675987d16df63834c55bcef70a18f01951c0e
Minindu20 <mininduh.20@cse.mrt.ac.lk>
2023-09-15 00:21:37 +0530

    local-change

--- src/routes/Admin.jsx
+++ src/routes/Admin.jsx
@@ -3,7 +3,8 @@ import './Css/Admin.css';
 import Nav from '../components/Dashboard/Nav';
 import Sidebar from '../components/Dashboard/Sidebar';
 import Widget from '../components/Widget/Widget';
-
+import GetUserPieChart from '../components/Librarian/GetUserPieChart';
+import GetBookPieChart from '../components/Librarian/GetBookPieChart';
 const Admin = () => {
   return (
     
@@ -23,7 +24,16 @@ const Admin = () => {
               <Widget 
                 type="recent"/>
           </div>
-          
+          <div className='card-block'>
+            <div className="card">
+              <h1>USER STATS</h1>
+              <GetUserPieChart/>
+              </div>
+            <div className="card">
+              <h1>BOOK STATS</h1>
+              <GetBookPieChart/>
+              </div>
+              </div>
         </div>
     </div>
   )
